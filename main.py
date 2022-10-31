87d9455f09ed9ec1f1deb5128db395b915d8f70a
Harsh Nannur <harsha2bits@gmail.com>
2022-10-30 20:07:56 -0500

    one video working blank is yet to be added

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from paho.mqtt import client as mqtt_client
 import time
+import vlc
 
+# VLC video path
+path_folder = "assets/"
+file_name = "1.mp4"
+path = path_folder+file_name
 
+# MQTT info
 broker = '192.168.1.113'
 port = 1883
 pubtopic = "python/mqttpub"
@@ -9,7 +15,6 @@ topicsub = "python/mqttsub"
 client_id = f'python-mqtt-1'
 username = 'harsha2bits'
 password = 'LeanderLion@1604'
-
 
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
@@ -25,36 +30,75 @@ def connect_mqtt():
     return client
 
 
-def publish(client):
-    msg_count = 0
-    while True:
-        time.sleep(1)
-        msg = f"messages: {msg_count}"
-        result = client.publish(pubtopic, msg)
-        # result: [0, 1]
-        status = result[0]
-        if status == 0:
-            print(f"Sent `{msg}` to topic `{pubtopic}`")
-        else:
-            print(f"Failed to send message to topic {pubtopic}")
-        msg_count += 1
+# def publish(client):
+#     msg_count = 0
+#     while True:
+#         time.sleep(1)
+#         msg = f"messages: {msg_count}"
+#         result = client.publish(pubtopic, msg)
+#         # result: [0, 1]
+#         status = result[0]
+#         if status == 0:
+#             print(f"Sent `{msg}` to topic `{pubtopic}`")
+#         else:
+#             print(f"Failed to send message to topic {pubtopic}")
+#         msg_count += 1
+
+
+def publish_complete(client):
+    msg = "done"
+    result = client.publish(pubtopic,msg)
+    status = result[0]
+    if status == 0:
+        print(f"Sent `{msg}` to topic `{pubtopic}`")
+    else:
+        print(f"Failed to send message to topic {pubtopic}")
 
 
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         print(client)
         print(userdata)
-        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        data = msg.payload.decode()
+        print(f"Received `{data}` from `{msg.topic}` topic")
+        if data == "1":
+            print("play_the_video")
+            play_video()  #Function to play video
+            print("Video played")
+            publish_complete(client)
 
     client.subscribe(topicsub)
     client.on_message = on_message
+
+
+def play_video():
+    # creating vlc media player object
+    media_player = vlc.MediaPlayer()
+    # toggling full screen
+    media_player.toggle_fullscreen()
+    # media object
+    media = vlc.Media(path)
+    # setting media to the media playergit
+    media_player.set_media(media)
+    media_player.play()
+    playing = set([1, 2, 3, 4])
+    play = True
+    while play:
+        time.sleep(0.5)
+        state = media.get_state()
+        if state in playing:
+            continue
+        else:
+            play = False
 
 
 def run():
     client = connect_mqtt()
     subscribe(client)
     client.loop_start()
-    publish(client)
+    # Play blank video in loop
+    while True:
+        None
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,13 +1,54 @@
-#import os
-#import terminal_video_player_py as tvp
-import vlc
+# #import os
+# import vlc
 path_folder ="assets/"
-file_name ="1.jpeg"
+file_name ="1.mp4"
 path =path_folder+file_name
-#os.open(path,3)
-#tvp(path)
+# #os.open(path,os.O_RDONLY)
+# print("playing video")
+# # creating vlc media player object
+# media = vlc.MediaPlayer(path)
+#
+# # start playing video
+# media.play()
+
+# importing vlc module
+import vlc
+
+# importing time module
+import time
+
 # creating vlc media player object
-media = vlc.MediaPlayer(path)
+media_player = vlc.MediaPlayer()
+
+# toggling full screen
+media_player.toggle_fullscreen()
+
+# media object
+media = vlc.Media(path)
+
+# setting media to the media playergit
+media_player.set_media(media)
+
+media_player.set_nsobject(self.GetHandle())
 
 # start playing video
+media_player.play()
+
+# wait so the video can be played for 5 seconds
+# irrespective for length of video
+#time.sleep(10)
+
+import vlc
+import time
+
+media = vlc.MediaPlayer("video.mp4")
 media.play()
+playing = set([1,2,3,4])
+play = True
+while play:
+    time.sleep(0.5)
+    state = media.get_state()
+    if state in playing:
+        continue
+    else:
+        play = False
